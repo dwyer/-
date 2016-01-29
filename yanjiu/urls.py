a8bf84380120b80bc6b5989c8d330bfee50c3a5b
Casey Dwyer <caseydwyer@gmail.com>
2016-01-30 01:53:21 +0800

    Consolidate cedict.urls into yanjiu.urls.

--- yanjiu/urls.py
+++ yanjiu/urls.py
@@ -4,11 +4,18 @@ from django.contrib import admin
 from django.conf.urls.static import static
 
 import audio.views
+import cedict.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^', include('cedict.urls')),
+    url(r'^$', cedict.views.index, name='cedict_index'),
+    url(r'^phrases/cn/(?P<simplified>.+)/$', cedict.views.phrase_view,
+        name='cedict_phrase_cn'),
+    url(r'^phrases/tw/(?P<traditional>.+)/$', cedict.views.phrase_view,
+        name='cedict_phrase_tw'),
+    url(r'^search$', cedict.views.phrase_list, name='cedict_search'),
+    url(r'^random$', cedict.views.random_phrase, name='cedict_random'),
     url(r'^audio/', include('audio.urls')),
 ]
 
