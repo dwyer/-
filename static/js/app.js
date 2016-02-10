81e5fe4e7bbd421b3b7439032249b87fff8d8de2
Casey Dwyer <caseydwyer@gmail.com>
2016-02-10 23:18:40 +0800

    app.js: Reformat and fix indentation.

--- static/js/app.js
+++ static/js/app.js
@@ -2,21 +2,21 @@
 
 (function () {
 
-  var app = angular.module('yanjiu', [
+  angular.module('yanjiu', [
     'ngMessageFormat',
     'ngRoute',
     'ngSanitize',
     'mgcrea.ngStrap.affix',
-  ]);
+  ])
 
 
-  app.config(['$httpProvider', function ($httpProvider) {
+  .config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.common['X-CSRFToken'] = CSRF_TOKEN;
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-  }]);
+  }])
 
 
-  app.config(['$routeProvider', function ($routeProvider) {
+  .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
     .when('/texts', {
       templateUrl: PARTIALS_DIR + 'text_list.html',
@@ -43,10 +43,10 @@
       controller: 'StarredPhrasesCtrl'//,
     })
     .otherwise({redirectTo: '/'});
-  }]);
+  }])
 
 
-  app.factory('phraseService', ['$http', function ($http) {
+  .factory('phraseService', ['$http', function ($http) {
     return {
       playAudio: function (phrase) {
         var url = '/audio/' + phrase + '.mp4';
@@ -67,10 +67,10 @@
         phrase.is_starred = !phrase.is_starred;
       }
     };
-  }]);
+  }])
 
 
-  app.directive('contenteditable', ['$sce', function ($sce) {
+  .directive('contenteditable', ['$sce', function ($sce) {
     return {
       restrict: 'A',
       require: '?ngModel',
@@ -88,10 +88,10 @@
         });
       }
     };
-  }]);
+  }])
 
 
-  app.filter('embeddedVideoUrl', ['$sce', function ($sce) {
+  .filter('embeddedVideoUrl', ['$sce', function ($sce) {
     var p = /^https?:\/\/www\.youtube\.com\/watch\?v=(\w+)$/;
     return function (input) {
       var m = p.exec(input);
@@ -100,14 +100,14 @@
       }
       return null;
     };
-  }]);
+  }])
 
 
   /**
    * Wrap Chinese sentences in a span.zh-sent tag.
    * TODO: lookup words.
    */
-  app.filter('processText', ['$sce', function ($sce) {
+  .filter('processText', ['$sce', function ($sce) {
 
     var cache = {};
 
@@ -143,45 +143,47 @@
       }
       return $sce.trustAsHtml(fragments.join(''));
     };
-  }]);
+  }])
 
 
-  app.controller('TextListCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('TextListCtrl', ['$scope', '$http', function ($scope, $http) {
     $http.get('/api/v1/texts?fields=id,title,video_url')
     .then(function (response) {
       $scope.texts = response.data.results;
     });
-  }]);
+  }])
 
 
-  app.controller('TextDetailCtrl', [
+  .controller('TextDetailCtrl', [
     '$scope', '$http', '$routeParams', 'phraseService',
     function ($scope, $http, $routeParams, phraseService) {
-      $scope.phraseService = phraseService;
       $scope.data = {};
-      $http.get('/api/v1/texts/' + $routeParams.id)
-      .then(function (response) {
-        $scope.text = response.data;
-        $scope.data.processedText = $scope.text.processed_text;
-        $scope.isWritable = $scope.text.owner.id == USER_ID;
-      });
+      $scope.editMode = false;
       $scope.phraseListPartialUrl = PARTIALS_DIR + 'phrase_list.html';
+      $scope.phraseService = phraseService;
       $scope.selection = null;
-      $scope.listener = function () {
+
+      $scope.selectPhrase = function (selection) {
         var oldSelection = $scope.selection;
-        $scope.selection = window.getSelection().toString();
+        $scope.selection = selection;
         if ($scope.selection.length > 0 && $scope.selection !== oldSelection) {
-          $http.get('/api/v1/phrases?traditional=' + encodeURIComponent($scope.selection))
+          $http.get('/api/v1/phrases?traditional='
+                    + encodeURIComponent($scope.selection))
           .then(function (response) {
             $scope.phrases = response.data.results;
           });
         }
       };
-      $scope.editMode = false;
+
+      $scope.listener = function () {
+        $scope.selectPhrase(window.getSelection().toString());
+      };
+
       $scope.toggleEditMode = function () {
         $scope.data.processedText = $scope.text.processed_text;
         $scope.editMode = !$scope.editMode;
       };
+
       $scope.save = function () {
         $scope.editMode = false;
         $scope.text.text = $scope.data.processedText;
@@ -191,106 +193,129 @@
           $scope.data.processedText = $scope.text.processed_text;
         });
       };
-    }]);
+
+      $http.get('/api/v1/texts/' + $routeParams.id)
+      .then(function (response) {
+        $scope.text = response.data;
+        $scope.data.processedText = $scope.text.processed_text;
+        $scope.isWritable = $scope.text.owner.id == USER_ID;
+      });
+    }
+  ])
 
 
-    app.controller('TextEditCtrl', [
-      '$scope', '$http', '$routeParams',
-      function ($scope, $http, $routeParams) {
-        $http.get(API_BASE_URL + 'texts/' + $routeParams.id)
-        .then(function (response) {
-          $scope.text = response.data;
-          if ($scope.text.owner.id != USER_ID) {
-            // TODO: raise error
-          }
-        });
-        $scope.finish = function () {
-          window.location.href = '#/texts/' + $routeParams.id;
-        };
-        $scope.submit = function (isValid) {
-          if (isValid) {
-            $http.put(API_BASE_URL + 'texts/' + $routeParams.id, $scope.text)
-            .then(function (response) {
-              $scope.finish();
-            }, function (response) {
-              $scope.error = response.data;
-            });
-          }
-        };
-      }]);
+  .controller('TextEditCtrl', [
+    '$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
 
+      $scope.finish = function () {
+        window.location.href = '#/texts/' + $routeParams.id;
+      };
 
-    app.controller('TextCreateCtrl', [
-      '$scope', '$http', '$routeParams',
-      function ($scope, $http, $routeParams) {
-        $scope.text = {};
-        $scope.finish = function () {
-          if (window.history.length > 1)
-            window.history.back();
-          else
-            window.location.href = '#/';
-        };
-        $scope.submit = function (isValid) {
-          if (isValid) {
-            $http.post(API_BASE_URL + 'texts', $scope.text)
-            .then(function (response) {
-              window.location.href = '#/texts/' + response.data.id;
-            }, function (response) {
-              $scope.error = response.data;
-            });
-          }
-        };
-      }]);
-
-    app.controller('SearchCtrl', [
-      '$scope', '$http', '$routeParams', 'phraseService',
-      function ($scope, $http, $routeParams, phraseService) {
-        $scope.phraseService = phraseService;
-        $scope.search_query = $routeParams.query;
-        $scope.phrases = {};
-        $scope.more = function () {
-          $http.get($scope.data.next).then(function (response) {
-            $scope.data = response.data;
-            for (var i in response.data.results)
-              $scope.phrases.push(response.data.results[i]);
+      $scope.submit = function (isValid) {
+        if (isValid) {
+          $http.put(API_BASE_URL + 'texts/' + $routeParams.id, $scope.text)
+          .then(function (response) {
+            $scope.finish();
+          }, function (response) {
+            $scope.error = response.data;
           });
         }
-        $http.get(API_BASE_URL + 'phrases?q='
-                  + encodeURIComponent($routeParams.query) + '&lang='
-                  + encodeURIComponent($routeParams.lang))
-        .then(function (response) {
-          $scope.data = response.data;
-          $scope.phrases = response.data.results;
-        });
-      }]);
+      };
 
-    app.controller('StarredPhrasesCtrl', [
-      '$scope', '$http', 'phraseService',
-      function ($scope, $http, phraseService) {
-        $scope.phraseService = phraseService;
-        $scope.phrases = {};
-        $scope.more = function () {
-          $http.get($scope.data.next).then(function (response) {
-            $scope.data = response.data;
-            for (var i in response.data.results)
-              $scope.phrases.push(response.data.results[i]);
+      $http.get(API_BASE_URL + 'texts/' + $routeParams.id)
+      .then(function (response) {
+        $scope.text = response.data;
+        if ($scope.text.owner.id != USER_ID) {
+          // TODO: raise error
+        }
+      });
+    }
+  ])
+
+
+  .controller('TextCreateCtrl', [
+    '$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
+      $scope.text = {};
+
+      $scope.finish = function () {
+        if (window.history.length > 1)
+          window.history.back();
+        else
+          window.location.href = '#/';
+      };
+
+      $scope.submit = function (isValid) {
+        if (isValid) {
+          $http.post(API_BASE_URL + 'texts', $scope.text)
+          .then(function (response) {
+            window.location.href = '#/texts/' + response.data.id;
+          }, function (response) {
+            $scope.error = response.data;
           });
         }
-        $http.get(API_BASE_URL + 'phrases?starred=true')
-        .then(function (response) {
+      };
+    }
+  ])
+
+
+  .controller('SearchCtrl', [
+    '$scope', '$http', '$routeParams', 'phraseService',
+    function ($scope, $http, $routeParams, phraseService) {
+      $scope.phraseService = phraseService;
+      $scope.phrases = {};
+      $scope.search_query = $routeParams.query;
+
+      $scope.more = function () {
+        $http.get($scope.data.next).then(function (response) {
           $scope.data = response.data;
-          $scope.phrases = response.data.results;
+          for (var i in response.data.results)
+            $scope.phrases.push(response.data.results[i]);
         });
       }
-    ]);
 
-    app.controller('SearchFormCtrl', ['$scope', function ($scope) {
-      $scope.lang = 'zh-tw';
-      $scope.search = function () {
-        window.location.href = ('#/search/' + encodeURIComponent($scope.lang)
-                                + '/' + encodeURIComponent($scope.query));
-      };
-    }]);
+      $http.get(API_BASE_URL + 'phrases?q='
+                + encodeURIComponent($routeParams.query) + '&lang='
+                + encodeURIComponent($routeParams.lang))
+                .then(function (response) {
+                  $scope.data = response.data;
+                  $scope.phrases = response.data.results;
+                });
+    }
+  ])
 
+
+  .controller('StarredPhrasesCtrl', [
+    '$scope', '$http', 'phraseService',
+    function ($scope, $http, phraseService) {
+      $scope.phraseService = phraseService;
+      $scope.phrases = {};
+
+      $scope.more = function () {
+        $http.get($scope.data.next).then(function (response) {
+          $scope.data = response.data;
+          for (var i in response.data.results)
+            $scope.phrases.push(response.data.results[i]);
+        });
+      }
+
+      $http.get(API_BASE_URL + 'phrases?starred=true')
+      .then(function (response) {
+        $scope.data = response.data;
+        $scope.phrases = response.data.results;
+      });
+    }
+  ])
+
+
+  .controller('SearchFormCtrl', ['$scope', function ($scope) {
+    $scope.lang = 'zh-tw';
+
+    $scope.search = function () {
+      window.location.href = ('#/search/' + encodeURIComponent($scope.lang)
+                              + '/' + encodeURIComponent($scope.query));
+    };
+  }]);
 
 })();
