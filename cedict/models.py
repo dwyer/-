15e126e150705a52dfb455c4c1ff0be465d55695
Casey Dwyer <caseydwyer@gmail.com>
2016-03-13 01:15:31 +0800

    cedict.models.Term: Add frequency field.

--- cedict/models.py
+++ cedict/models.py
@@ -9,6 +9,7 @@ class Term(models.Model):
     traditional = models.CharField(max_length=255, blank=False)
     simplified = models.CharField(max_length=255, blank=False)
     pinyin = models.CharField(max_length=255, blank=False)
+    frequency = models.FloatField(default=0)
 
     @property
     def pinyin_unicode(self):
